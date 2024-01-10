ec572dfae9aa0f3047f32644ec63359161027bae
bi4528 <36712965+bi4528@users.noreply.github.com>
2024-01-10 07:14:27 +0100

    Nadaljevanje dela na vmesniku

--- src/app/cocktail.service.ts
+++ src/app/cocktail.service.ts
@@ -1,18 +1,20 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Observable, of} from 'rxjs';
 import { Cocktail } from './cocktail.model';
+import {catchError} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CocktailService {
   // private apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
-  private apiUrl = 'http://localhost:8080/v1/koktejli/name/margarita';
+  private apiUrl = 'http://34.89.140.13/koktejli/v1/koktejli/name/margarita';
 
   constructor(private http: HttpClient ) { }
 
-  getCocktails(): Observable<{drinks: Cocktail[]}> {
-    return this.http.get<{drinks: Cocktail[]}>(this.apiUrl);
+  getCocktails(name?: string): Observable<{drinks: Cocktail[]}> {
+    const url = name ? `${this.apiUrl}${name}` : this.apiUrl + 'margarita';
+    return this.http.get<{drinks: Cocktail[]}>(url);
   }
 }
--- src/app/seznam-koktejlov/seznam-koktejlov.component.ts
+++ src/app/seznam-koktejlov/seznam-koktejlov.component.ts
@@ -2,23 +2,53 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { CocktailService } from '../cocktail.service';
+import { UsersService } from '../users.service';
+import { UsercocktailService } from '../usercocktail.service';
 import { Cocktail } from '../cocktail.model';
+import {Usercocktail} from "../usercocktail.model";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-seznam-koktejlov',
   templateUrl: './seznam-koktejlov.component.html',
   styleUrls: ['./seznam-koktejlov.component.css'],
   standalone: true,
-  imports: [CommonModule, HttpClientModule] // Dodajte HttpClientModule tukaj
+  imports: [CommonModule, HttpClientModule, FormsModule] // Dodajte HttpClientModule tukaj
 })
 export class SeznamKoktejlovComponent implements OnInit {
   cocktails: Cocktail[] = [];
+  users: string[] = [];
+  usercocktails: Usercocktail[] = [];
+  searchQuery: string = '';
 
-  constructor(private cocktailService: CocktailService) { }
+  constructor(private cocktailService: CocktailService, private usersService: UsersService, private usercocktailService: UsercocktailService) { }
 
   ngOnInit(): void {
     this.cocktailService.getCocktails().subscribe(data => {
       this.cocktails = data.drinks;
     });
+    this.usersService.getUsers().subscribe(data => {
+      this.users = data;
+      this.selectedUser = this.users[0]
+      this.usercocktailService.getUsercocktails(this.selectedUser).subscribe(data1 => {
+        this.usercocktails = data1;
+      });
+    });
   }
+
+  selectedUser: string | null = null;
+
+  selectUser(user: string): void {
+    this.selectedUser = user;
+    this.usercocktailService.getUsercocktails(user).subscribe(data => {
+      this.usercocktails = data;
+    });
+  }
+
+  searchCocktails(): void {
+    this.cocktailService.getCocktails(this.searchQuery).subscribe(data => {
+      this.cocktails = data.drinks;
+    });
+  }
+
 }
